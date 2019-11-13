43796875a8b18b9dfb0715466d0e4de5e7863723
Dmitry-Konopelko <Dmitry_Konopelko@outlook.com>
2019-11-13 07:00:48 +0300

    5

---
+++ WebKonopelko/WebKonopelko.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebKonopelko.DAL.Entities;
+
+namespace WebKonopelko.DAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        { }
+    }
+}
---
+++ WebKonopelko/WebKonopelko.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace WebKonopelko.DAL.Entities
+{
+    public class ApplicationUser:IdentityUser
+    {
+    }
+}
